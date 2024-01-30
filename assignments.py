04cbcca9ac6e51f9ef9ce39d58c5ce70aec001d0
hudaa127 <157005693+hudaa127@users.noreply.github.com>
2024-01-31 02:16:06 +0500

    Add files via upload

---
+++ assignments.py
@@ -0,0 +1,25 @@
+Python 3.10.10 (tags/v3.10.10:aad5f6a, Feb  7 2023, 17:20:36) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> height=float(input("Enter value for height of a triangle:"))
+Enter value for height of a triangle:77.33
+>>> base=float(input("Enter value for base of a triangle:"))
+Enter value for base of a triangle:11.44
+>>> area=(0.5)*base*height
+>>> print("the area of the triangle is", area)
+the area of the triangle is 442.32759999999996
+>>> 
+>>> 
+>>> radius = float(input("Enter value of radius of a circle: "))
+Enter value of radius of a circle: 12.33
+>>> area = (3.142)**radius
+>>> print("the area of a circle is: ", area)
+the area of a circle is:  1350679.1880308522
+>>> 
+>>> 
+>>> num1= int(input("Enter number 1: "))
+Enter number 1: 12
+>>> num2= int(input("Enter number 2: "))
+Enter number 2: 1444
+>>> avg=(num1+num2)/2
+>>> print("the average of two number is : ", avg)
+the average of two number is :  728.0
